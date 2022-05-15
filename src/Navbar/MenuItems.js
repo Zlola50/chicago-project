b5ab724fa09eeb5c24cfdcfe2a70e2b580825581
zainab <zainab@gmail.com>
2022-05-14 13:07:20 -1200

    Demo Commit

---
+++ src/Navbar/MenuItems.js
@@ -0,0 +1,29 @@
+export const MenuItems =[
+    {
+        title: 'Home',
+        url: "/",
+        cName: "nav-links"
+    },
+    {
+        title: 'Services',
+        url: "/",
+        cName: "nav-links"
+    },
+    {
+        title: 'About-Us',
+        url: "/",
+        cName: "nav-links"
+    },
+    {
+        title: 'Contact',
+        url: "/",
+        cName: "nav-links"
+    },
+    {
+        title: 'SIGN UP',
+        url: "/",
+        cName: "nav-links-mobile"
+    }
+]
+
+export default MenuItems
